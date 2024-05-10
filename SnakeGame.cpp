2c2ae398b88f740678fbf1546020b7c19f60a933
tina445 <gmltjq4731@icloud.com>
2024-05-11 02:28:34 +0900

    snake 이동 기능 완성

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -1,41 +1,64 @@
-#include "GameMap.cpp"
-#include "InputManager.cpp"
+#include "GameMap.h"
+#include "InputManager.h"
+#include "Snake.h"
 #include <ncurses.h>
 
 #define TICKSPEED 2 // tick per second
-unsigned int tick{};
 
-using namespace gamemap;
-using namespace inputmanager;
+unsigned int tick; // 시간 경과 기록 (앞서 설정한 tick 단위)
+int score; // 점수
+bool isOver{false}; // 게임 오버 여부 check
+
+void Snake::Dead() { isOver = true; }
+
+int SnakeGame() {
+    // Start()
+    tick = 0;
+
+    GameMap map;
+    InputManager key_input;
+    Snake snake{{STARTPOS, STARTPOS}, SNAKE_DEFAULT};
+
+    map.map[STARTPOS][STARTPOS] = HEAD;
+    for (int i = 1; i < SNAKE_DEFAULT; i++) {
+        map.map[STARTPOS][STARTPOS + i] = BODY;
+    }
+
+    char key;
+
+    // Update()
+    while (1) {
+        tick++;
+        key = key_input.keyDown(); // 방향키 입력 처리
+
+        snake.moveSnake(map.map, key); // snake 이동 처리
+
+        map.printMap();
+
+        mvprintw(0, 0, "%u", tick);
+        mvprintw(0, 5, "input: %c", key);
+        delch();
+        refresh();
+
+        if (tick > 200 || isOver) break;
+        napms(1000 / TICKSPEED);
+    }
+    
+    return -1;
+}
 
 int main(int agrc, char *argv[]) {
-    // init window
+    // Strat()
     initscr();
     noecho();
     nodelay(stdscr, true);
     keypad(stdscr, true);
     curs_set(0);
 
-    // Awake()
-    GameMap map;
-    InputManager key_input;
-
-    // Start()
-    char key;
-
     // Update()
     while (1) {
-        tick++;
-        map.printMap();
-        mvprintw(0, 0, "%u", tick);
-
-        key = key_input.keyDown();
-        mvprintw(0, 5, "input: %c", key);
-        delch();
-        refresh();
-
-        if (tick > 200) break;
-        napms(1000 / TICKSPEED);
+        int x = SnakeGame();
+        if (x == -1) break;
     }
     endwin();
 }
