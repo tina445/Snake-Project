2327a2b8eef2664b0044354d7c5a869ed7822e5f
tina445 <gmltjq4731@icloud.com>
2024-05-13 02:43:38 +0900

    파일 입력을 통한 맵 초기화 기능 구현

--- GameMap.cpp
+++ GameMap.cpp
@@ -1,5 +1,7 @@
 #include <ncurses.h>
 #include <vector>
+#include <string>
+#include <fstream>
 #include "GameMap.h"
 #include "objectid.h"
 
@@ -36,8 +38,8 @@ GameMap::GameMap(int startpos, int snakeSize) {
     init_color(darkgray, 200, 200, 200); // 모서리에 사용
     init_color(lightblue, 90, 700, 1000); // snake body에 사용
     init_color(blue, 90, 320, 1000); // snake head에 사용
-    init_color(red, 1000, 240, 240);
-    init_color(purple, 400, 200, 800);
+    init_color(red, 1000, 240, 240); // growth 아이템에 사용
+    init_color(purple, 400, 200, 800); // poison 아이템에 사용
 
     // 색상 팔레트 지정
     init_pair(space, COLOR_BLACK, green);
@@ -50,7 +52,50 @@ GameMap::GameMap(int startpos, int snakeSize) {
     init_pair(poison, COLOR_BLACK, purple);
 }
 
-GameMap::GameMap() {}
+GameMap::GameMap() {
+    createMap(); // 스테이지에 해당하는 맵 생성
+
+    // 색상 정의
+    init_color(green, 700, 1000, 700);
+    init_color(lightgreen, 850, 1000, 850); // 빈 공간의 격자무늬
+    init_color(gray, 400, 400, 400); // 벽에 사용
+    init_color(darkgray, 200, 200, 200); // 모서리에 사용
+    init_color(lightblue, 90, 700, 1000); // snake body에 사용
+    init_color(blue, 90, 320, 1000); // snake head에 사용
+    init_color(red, 1000, 240, 240); // growth 아이템에 사용
+    init_color(purple, 400, 200, 800); // poison 아이템에 사용
+
+    // 색상 팔레트 지정
+    init_pair(space, COLOR_BLACK, green);
+    init_pair(spaceg, COLOR_BLACK, lightgreen);
+    init_pair(wall, COLOR_BLACK, gray);
+    init_pair(Iwall, COLOR_BLACK, darkgray);
+    init_pair(body, COLOR_BLACK, lightblue);
+    init_pair(head, COLOR_BLACK, blue);
+    init_pair(growth, COLOR_BLACK, red);
+    init_pair(poison, COLOR_BLACK, purple);
+}
+
+void GameMap::createMap() {
+    ifstream readfile;
+    readfile.open("stage/world2-1.map"); // 맵에 대한 정보가 담긴 파일 열기
+
+    vector<int> tmp; // map에 넣을 임시 1차원 벡터 (empty vector)
+    string line; // 한 행에 대한 정보를 담을 임시 문자열
+
+    if (readfile.is_open()) {
+        for (int i = 0; !readfile.eof(); i++) {
+            map.push_back(tmp);
+            getline(readfile, line);
+        
+            for (char obj : line) {
+                map[i].push_back(obj % '0'); // 맵에 해당하는 오브젝트 저장
+            }
+        }   
+    }
+
+    readfile.close();
+}
 
 void GameMap::printMap() { // map 출력
     move(1, 0);
@@ -63,3 +108,10 @@ void GameMap::printMap() { // map 출력
     }
 }
 
+int GameMap::ySize() { // 맵의 세로 길이 반환
+    return map.size();
+}
+
+int GameMap::xSize() { // 맵의 가로 길이 반환
+    return map[0].size();
+}
